57d486be731deb44a28251b3cfe4b8f8eee05c6c
Yash Guard <yashguard>
2023-08-04 12:52:03 +0530

    Error solved

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import SignIn from "../Components/SignIn";
 
 const Login = () => {
   return (
     <div>
-      
+      <SignIn
+        title="Sign in"
+        des="Sign up or Sign in to access your orders, special offers, health"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
